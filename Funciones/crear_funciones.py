e4b4a1e4896b430e3be5d0093a2b1f77f3c5d453
Johan Andres Rojas <dj.johan87@gmail.com>
2023-02-09 21:22:10 -0500

    corregida funcion saludo

--- Funciones/crear_funciones.py
+++ Funciones/crear_funciones.py
@@ -2,14 +2,21 @@
 
 nombre = input("Ingresa el nombre: ")
 apellido = input("Ingresa el apellido: ")
+sexo = input("Ingrese su genero: ")
 
 
+def saludar(nombre,apellido,sexo):
+    sexo = sexo.lower()
+#    if (nombre = "" or apellido == "" or sexo == ""):
+#         print("Hola,  como estas?")
+    if  (sexo == "mujer"):
+        genero = "Señora"
+    elif (sexo == "hombre"):
+        genero = "Señor"
+    else: 
+        genero = "corazon"
+         
+    print(f"Hola {nombre}  {apellido},  como estas mi {genero}?")
 
-def saludar(nombre,apellido):
-    print(f"Hola {nombre}  {apellido}, como estas?")
 
-
-saludar(nombre,apellido)
-
-
-
+saludar(nombre,apellido,sexo)
